a234bdcb4f193fae5b7309b910a710ea8a7f36de
Hannah Angle <hannahcangle@gmail.com>
2020-08-31 12:16:03 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -24,6 +24,19 @@ class Student
   
   def self.drop_table 
     sql = "DROP TABLE IF EXISTS students"
+     DB[:conn].execute(sql)
+   end 
+   
+   def save 
+     sql = <<-SQL
+      INSERT INTO students (name, grade)
+      VALUES (?, ?)
+    SQL
+    
+    DB[:conn].execute(sql, self.name, self.grade)
+  end 
+      
+      
     
       
 end
