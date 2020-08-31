6e9f760706603898fe94f5f5dc4132ce8cc699c0
Hannah Angle <hannahcangle@gmail.com>
2020-08-31 12:17:35 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -34,6 +34,8 @@ class Student
     SQL
     
     DB[:conn].execute(sql, self.name, self.grade)
+    
+    @ID = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
   end 
       
       
